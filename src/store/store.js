49c8bfba427b60c0f89a7ba5fafb263f013cea55
NR0617 <nara20210617@gmail.com>
2022-11-05 23:56:24 +0900

    fix : CSS 수정

--- src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import dataSlice from './dataSlice';
-
-const store = configureStore({
-  reducer: {
-    detaildata: dataSlice.reducer,
-  },
-});
-export default store;
